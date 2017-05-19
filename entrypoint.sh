f5f165426bfa9242338ea7e8d5182cf112828a7a
lordillusions <lordillusions@gmail.com>
2017-05-19 08:15:39 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -26,8 +26,8 @@ opts=(
 	dc_other_hostnames ''
 	# dc_relay_domains "ibm.com;*.ibm.com"
 	# dc_relay_domains ''
-	# dc_relay_nets "$(ip addr show dev `ls -1 /sys/class/net | tail -1` | awk '$1 == "inet" { print $2 }' | xargs | sed 's/ /:/g')${RELAY_NETWORKS}"
-	dc_relay_nets ''
+	dc_relay_nets "$(ip addr show dev `ls -1 /sys/class/net | tail -1` | awk '$1 == "inet" { print $2 }' | xargs | sed 's/ /:/g')${RELAY_NETWORKS}"
+	# dc_relay_nets ''
 )
 
 if [ "$DISABLE_IPV6" ]; then 
